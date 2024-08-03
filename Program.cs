269bdfd0cebd61a9ebd779d9e3d8fdb86c1a19a0
Farin Jakate <150505333+farinjakate@users.noreply.github.com>
2024-08-03 14:26:47 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using MathLibrary;
+
 Console.WriteLine("Hello, World!");
-Console.ReadLine();
+
+Calculator c1 = new Calculator();
+c1.Add();
+
 
 
 static void print()
 {
-    Console.WriteLine();
+    Console.WriteLine("Print()Method called");
 }
