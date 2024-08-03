5e4d0474bb99451d43c925b9f205baf9297c9c81
Farin Jakate <150505333+farinjakate@users.noreply.github.com>
2024-08-03 15:06:39 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿using MathLibrary;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine("Hello, World!");
-
-Calculator c1 = new Calculator();
-c1.Add();
-
-
-
-static void print()
+namespace _2_GenerationOperators
 {
-    Console.WriteLine("Print()Method called");
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> numbers1 = Enumerable.Empty<string>();
+            Console.WriteLine($"{numbers1.Count()}");//// 0
+
+            IEnumerable<int> numbers2 = Enumerable.DefaultIfEmpty<int>(numbers1);
+            Console.WriteLine($"{numbers2.Count()} :{numbers2.ElementAt(0)}");
+
+
+           IEnumerable<int> numbers3=IEnumerable.Repeat(5, 10);
+            foreach(var item in numbers3)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            
+            Console.ReadLine();
+
+
+        }
+    }
 }
