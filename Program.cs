760db26a40a8c48d2a92bd4dbdd46c5332a6e76e
Farin Jakate <150505333+farinjakate@users.noreply.github.com>
2024-08-23 17:33:38 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,102 +4,192 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _5_TypeConversion
+namespace _8_ConditionalStatementsExamples
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int i = 10, j = 10;
-            Console.WriteLine(i + j);
-            //20
+          {
+            //    Console.WriteLine("Please enter first number");
+            //    int first = int.Parse(Console.ReadLine());
 
-            //int=>string
-            string a = i.ToString();
-            string b = j.ToString();
-            Console.WriteLine(a + b);
+            //    Console.WriteLine("Please enter second number");
+            //    int second = int.Parse(Console.ReadLine());
 
+            //    Console.WriteLine("Please enter third number");
+            //    int third = int.Parse(Console.ReadLine());
 
-            byte b1 = 10;
-            short s1 = b1;//byte->short
-            Console.WriteLine(s1);//10
-
-            float f1 = s1; //short to float
-            Console.WriteLine(f1);//10
-
-            //int i1 = f1; // float -> int
-            int i1 = (int)f1;// due to loss of data thats why explicit conversion
-            Console.WriteLine(f1);
-             
-            f1 = 25.55f;
-            i1 = (int)f1;//float->int
-            Console.WriteLine(i1);//25
-
-            string s = "100";
-            // i1 =(int) s;// string ->int
-            i1 = Convert.ToInt32(s);//convert class
-            Console.WriteLine(i1);//100
-
-            s = "true"; 
-            bool bb = Convert.ToBoolean(s);//string->bool
-            Console.WriteLine(bb);
-
-
-            
-            //parse() method throws error when string is not in correct format
-            s = "777";
-            i1 = int.Parse(s);//string->int
-            Console.WriteLine(i1);//777
-            
-            // s = "XYZ";
-            //i1 = int.Parse(s);//runtime error
-            //Console.WriteLine(i1);
-
-            //TryParse() -> it hadles the runtime error
-            /*Convert and return Conversion status along with converted
-            value .
-            in case of runtime error it returns false as a status 
-            and default value as output
-            default value of bool is false
-            default value of int is 0
-            default value of string is null*/
-
-            s = "2000";
-            bb = int.TryParse(s, out i1);
-            Console.WriteLine($"Status :{bb} Result :{i1}");
-            //Status :True Result :2000
-
-
-            s = "XYZ";
-            bb = int.TryParse(s, out i1);
-            Console.WriteLine($"Status :{bb} Result :{i1}");
-            //Status :False Result :0
-
-            s = "ABC";
-            bb = int.TryParse(s, out i1);
-            Console.WriteLine($"Status :{bb} Result :{i1}");
-
-            s = "true";
-            bool b3 = Convert.ToBoolean(s);
-            Console.WriteLine(b3);
-
-
-
-            s = "true";
-            bool b4 = Convert.ToBoolean(s);
-            Console.WriteLine(b4);
+            //    Console.WriteLine($"You enterd :{first},{second},{third}");
 
 
 
 
 
-            decimal X = 10;
-            long l = (long)X;//decimal ->long
-            Console.WriteLine(l);//10
+            #region nested is else
+
+            //if (first > second)
+            //{
+            //    if (first > third)
+            //    {
+            //        Console.WriteLine($"{first} is greater number");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{third} is greater number");
+            //    }
+            //}   
+            //else if (second > first)
+            //{
+            //    if (second > third)
+            //    {
+            //        Console.WriteLine($"{second} is greater number");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{third} is greater number");
+            //    }
+            //}
+            //else
+            //{
+            //    if (first > third)
+            //    {
+            //        Console.WriteLine($"{first}, {second} are greater number");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{third} is greater number");
+            //    }
+            //}
 
 
+
+
+            #endregion nested is else
+
+
+            #region nested Ternary operator
+            //ternary perator
+            // //int number = (first > second) ? first : second;
+
+
+            //int number = (first > second) ?
+            //  ((first > third) ? first : third) :
+            //((second > third) ? second : third);
+
+            //Console.WriteLine($"{number} is greater number");
+
+
+
+            #endregion nested Ternary operator
+
+
+            #region divisible 3 & 5
+
+            //if input number is divisible by 3=THREE
+            //iF input number is divisible by 5=FIVE
+            //if input number is divisible by 3 & 5 =THREEFIVE
+
+            //Console.WriteLine("Please Enter a number");
+            //int num = int.Parse(Console.ReadLine());
+
+
+
+            //if (num % 3 == 0 && num % 5 == 0)
+            //{
+            //    Console.WriteLine("THREE AND FIVE");
+            //}
+            //else if (num % 3 == 0)
+            //{
+            //    Console.WriteLine("THREE");
+            //}
+            //else if (num % 5 == 0)
+            //{
+            //    Console.WriteLine("FIVE");
+            //}
+
+            #endregion divisible 3 & 5
+
+
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= 5; j++)
+            //    { Console.Write($"{j}\t")
+            //            }
+            //    Console.WriteLine();
+            //}
+
+            #region even/odd number
+
+            // Ternary operator
+
+            //Console.WriteLine("Please enter a number");
+            //int num = int.Parse(Console.ReadLine());
+
+            //string result = (num % 2 == 0) ? "Even" : "Odd";
+
+            //Console.WriteLine($"{num} is {result} number");
+
+            #endregion even / odd
+
+
+
+            #region grade meaning
+
+            //Console.WriteLine("please enter a number");
+            //char grade = char.Parse(Console.ReadLine());
+
+            //switch (grade)
+            //{
+            //    case 'A':
+
+            //            Console.WriteLine($"{grade}:Distinction");
+            //            break;
+
+            //    case 'B':
+
+            //            Console.WriteLine($"{grade}: First class");
+            //            break;
+
+            //    case 'C':
+
+            //            Console.WriteLine($"{grade}:second class");
+            //            break;
+
+            //    case 'F':
+
+            //            Console.WriteLine($"{grade}: Failed"); 
+            //            break;
+
+            //    default:
+
+            //        Console.WriteLine($"{grade} : Invalid Grade");
+            //        break;
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion grade meaning
 
             Console.ReadLine();
+
+
+
+
+
+
 
 
 
